e46290edb431472186d4e4692f5a76b3b4ae830c
Samuel L Silva <samuellalau@hotmail.com>
2024-01-03 21:48:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="app">
+			<Header title="Cod3r - Base do Conhecimento" :hideToggle="false"/>
+			<Menu />
+			<Content />
+			<Footer />
+</div>
+</template>
+
+<script>
+import Header from './componets/template/Header.vue'
+import Menu from './componets/template/Menu.vue'
+import Content from './componets/template/Content.vue'
+import Footer from './componets/template/Footer.vue'
+
+export default {
+		name: "App",
+		components: { Header, Menu, Content, Footer }
+	}
+
+</script>
+
+<style>
+		* {
+		font-family: "Lato", sans-serif;
+	}
+
+	body {
+		margin: 0;
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas:
+			"header header"
+			"menu content"
+			"menu footer";
+	}
+</style>
